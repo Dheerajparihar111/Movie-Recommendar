d9577bec5e9213c5ee4d8037e45ba31d6b85a74d
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-04-08 22:30:37 +0530

    added Menu System

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
         addMovies();
         System.out.println("Movie system initialized");
+
     }
 
     static void addMovies() {
@@ -16,4 +17,23 @@ public class Main {
         movies.add(new Movie("Interstellar", "SciFi", 8.6, 2014));
         movies.add(new Movie("3 Idiots", "Comedy", 8.4, 2009));
     }
+
+    static Scanner input = new Scanner(System.in);
+
+    int choice;
+
+do {
+        System.out.println("\n===== MOVIE SYSTEM =====");
+        System.out.println("1 View Movies");
+        System.out.println("2 Exit");
+
+        choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                showAllMovies();
+                break;
+        }
+
+    } while (choice != 2);
 }
