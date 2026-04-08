3aa05b51c5c1614e9b4e2342db68b6d6136bcb1c
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-04-08 22:24:13 +0530

    add Movie class and type

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+class Movie {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    String name;
+    String genre;
+    double rating;
+    int year;
+
+    Movie(String name, String genre, double rating, int year) {
+        this.name = name;
+        this.genre = genre;
+        this.rating = rating;
+        this.year = year;
+    }
+
+    void display() {
+        System.out.println(name + " | " + genre + " | ⭐ " + rating + " | " + year);
+    }
 }
