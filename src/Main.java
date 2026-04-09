6bac56b38f028f57b06392b2177ea051f0888711
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-04-09 19:35:33 +0530

    updated the Main class for better user interface

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,60 @@ import java.util.*;
 
 public class Main {
 
-    static ArrayList<Movie> movies = new ArrayList<>();
-
     public static void main(String[] args) {
 
-        addMovies();
-        System.out.println("Movie system initialized");
+        Scanner input = new Scanner(System.in);
+        MovieService service = new MovieService();
 
+        int choice;
+
+        do {
+
+            System.out.println("\n===== MOVIE RECOMMENDATION SYSTEM =====");
+            System.out.println("1 View All Movies");
+            System.out.println("2 Search Movie");
+            System.out.println("3 Filter by Genre");
+            System.out.println("4 Recommend Movies");
+            System.out.println("5 Top Rated Movies");
+            System.out.println("6 Exit");
+
+            System.out.print("Enter choice: ");
+            choice = input.nextInt();
+            input.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    service.showAllMovies();
+                    break;
+
+                case 2:
+                    System.out.print("Enter movie name: ");
+                    service.searchMovie(input.nextLine());
+                    break;
+
+                case 3:
+                    System.out.print("Enter genre: ");
+                    service.filterByGenre(input.nextLine());
+                    break;
+
+                case 4:
+                    System.out.print("Enter preferred genre: ");
+                    service.recommendMovies(input.nextLine());
+                    break;
+
+                case 5:
+                    service.topRatedMovies();
+                    break;
+
+                case 6:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+        } while (choice != 6);
     }
-
-    static void addMovies() {
-
-        movies.add(new Movie("Inception", "SciFi", 8.8, 2010));
-        movies.add(new Movie("Interstellar", "SciFi", 8.6, 2014));
-        movies.add(new Movie("3 Idiots", "Comedy", 8.4, 2009));
-    }
-
-    static Scanner input = new Scanner(System.in);
-
-    int choice;
-
-do {
-        System.out.println("\n===== MOVIE SYSTEM =====");
-        System.out.println("1 View Movies");
-        System.out.println("2 Exit");
-
-        choice = input.nextInt();
-
-        switch (choice) {
-            case 1:
-                showAllMovies();
-                break;
-        }
-
-    } while (choice != 2);
 }
