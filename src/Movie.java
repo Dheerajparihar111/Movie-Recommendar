042343c849a956ab7cc091b049f7533b19f7996a
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-04-09 19:33:21 +0530

    upgraded the Movie class for storing data

--- src/Movie.java
+++ src/Movie.java
@@ -1,18 +1,23 @@
 class Movie {
 
-    String name;
-    String genre;
-    double rating;
-    int year;
+    private String name;
+    private String genre;
+    private double rating;
+    private int year;
 
-    Movie(String name, String genre, double rating, int year) {
+    public Movie(String name, String genre, double rating, int year) {
         this.name = name;
         this.genre = genre;
         this.rating = rating;
         this.year = year;
     }
 
-    void display() {
+    public String getName() { return name; }
+    public String getGenre() { return genre; }
+    public double getRating() { return rating; }
+    public int getYear() { return year; }
+
+    public void display() {
         System.out.println(name + " | " + genre + " | ⭐ " + rating + " | " + year);
     }
 }
